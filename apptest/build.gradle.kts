cdd4b5d8539622b92cc99539606870903da3f1da
dinaabdelnabybarah <dina.abdelnaby.barah@gmail.com>
2025-06-10 00:45:09 +0300

    Add apptest/build.gradle.kts 1749505508666

---
+++ apptest/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("com.android.application") version "8.0.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("dagger.hilt.android.plugin") version "2.44" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
